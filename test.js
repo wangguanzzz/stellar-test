d0d512bfd31ae823e443b96c559ac99135c4fd8d
Christopher Wang <wangguanzzz@126.com>
2018-03-18 14:47:30 +0000

    create account

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
 const StellarSdk = require('stellar-sdk')
 var pair = StellarSdk.Keypair.random()
 
-console.log(pair.secret())
-console.log(pair.publicKey())
+//console.log(pair.secret())
+//SA62M3IGLCBRCJSPWQAAECMETUYS4QOMK35DK27LZESQHSKSIZJXOJSC
+//console.log(pair.publicKey())
+const PUBLICKEY = 'GA7DOPRIFVDLPOJ65ZZT4EM55XH3YHXXIM2YQRIBNUCTQEKNYLAM3C5J'
+
+var request = require('request');
+request.get({
+  url: 'https://horizon-testnet.stellar.org/friendbot',
+  qs: { addr: PUBLICKEY },
+  json: true
+}, function(error, response, body) {
+  if (error || response.statusCode !== 200) {
+    console.error('ERROR!', error || body);
+  }
+  else {
+    console.log('SUCCESS! You have a new account :)\n', body);
+  }
+});
